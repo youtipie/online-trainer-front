a80034d508e6fe1214cede367dd99caad6f8bd3c
youtipie18 <youtipie@gmail.com>
2024-06-11 21:55:51 +0300

    Add horizontal scroll feature to footer when there is more than 5 items

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,78 +1,61 @@
-import React, {useState} from 'react';
+import React, {useEffect} from 'react';
 import progressIcon from "./images/progress-icon.png";
 import trainIcon from "./images/train-icon.png";
 import profileIcon from "./images/profile-icon.png";
 import eatIcon from "./images/eat-icon.png";
 import rankIcon from "./images/rank-icon.png";
 import "./footer.css";
-import {Link} from "react-router-dom";
+import {Link, useLocation} from "react-router-dom";
 
 const footerItems = [
+    {img: eatIcon, route: "/temp", text: "temp"},
     {img: progressIcon, route: "/progress", text: "Progress"},
     {img: trainIcon, route: "/train", text: "Train"},
     {img: profileIcon, route: "/profile", text: "Profile"},
     {img: eatIcon, route: "/eat", text: "Eat"},
-    {img: rankIcon, route: "/rank", text: "Rank"}
+    {img: rankIcon, route: "/rank", text: "Rank"},
+    {img: eatIcon, route: "/temp", text: "temp"}
+
 ]
 
 const Footer = () => {
-    const [currentPage, setCurrentPage] = useState("/profile");
+    const currentPage = useLocation();
+
+    useEffect(() => {
+        const scrollContainer = document.querySelector('.scroll-container');
+        if (scrollContainer && footerItems.length) {
+            const selectedElement = document.querySelector('.selected');
+            if (selectedElement) {
+                const containerWidth = scrollContainer.offsetWidth;
+                const elementWidth = selectedElement.offsetWidth;
+                const elementOffsetLeft = selectedElement.offsetLeft;
+
+                const scrollPosition = elementOffsetLeft - (containerWidth / 2) + (elementWidth / 2);
+
+                scrollContainer.scrollTo({
+                    left: scrollPosition,
+                    behavior: 'smooth'
+                });
+            }
+        }
+    }, []);
 
     return (
         <section id="footer">
             <footer>
-                {footerItems.map(item =>
-                    <div key={item.route} className="footer-item">
-                        <Link
-                            className={"link" + (currentPage === item.route ? " selected" : "")}
-                            to={item.route}
-                            onClick={() => setCurrentPage(item.route)}
-                        >
-                            <img src={item.img} alt={`${item.text} icon`}/>
-                            <p>{item.text}</p>
-                        </Link>
-                    </div>
-                )}
-                {/*<div className="footer-item">*/}
-                {/*    <Link*/}
-                {/*        className="link"*/}
-                {/*        to="/training"*/}
-                {/*        onClick={() => setCurrentPage("/")}*/}
-                {/*    >*/}
-                {/*        <img src={trainIcon} alt="training icon"/>*/}
-                {/*        <p>Training</p>*/}
-                {/*    </Link>*/}
-                {/*</div>*/}
-                {/*<div className="footer-item">*/}
-                {/*    <Link*/}
-                {/*        className="link"*/}
-                {/*        to="/profile"*/}
-                {/*        onClick={() => setCurrentPage("/")}*/}
-                {/*    >*/}
-                {/*        <img src={profileIcon} alt="profile icon"/>*/}
-                {/*        <p>Profile</p>*/}
-                {/*    </Link>*/}
-                {/*</div>*/}
-                {/*<div className="footer-item">*/}
-                {/*    <Link*/}
-                {/*        className="link"*/}
-                {/*        to="/eat"*/}
-                {/*        onClick={() => setCurrentPage("/")}*/}
-                {/*    >*/}
-                {/*        <img src={eatIcon} alt="eat icon"/>*/}
-                {/*        <p>Eat</p>*/}
-                {/*    </Link>*/}
-                {/*</div>*/}
-                {/*<div className="footer-item">*/}
-                {/*    <Link*/}
-                {/*        className="link"*/}
-                {/*        to="/rank"*/}
-                {/*        onClick={() => setCurrentPage("/")}*/}
-                {/*    >*/}
-                {/*        <img src={rankIcon} alt="rank icon"/>*/}
-                {/*        <p>Rank</p>*/}
-                {/*    </Link>*/}
-                {/*</div>*/}
+                <div className="scroll-container">
+                    {footerItems.map(item =>
+                        <div key={item.route} className="footer-item">
+                            <Link
+                                className={"link" + (currentPage.pathname === item.route ? " selected" : "")}
+                                to={item.route}
+                            >
+                                <img src={item.img} alt={`${item.text} icon`}/>
+                                <p>{item.text}</p>
+                            </Link>
+                        </div>
+                    )}
+                </div>
             </footer>
         </section>
     );
