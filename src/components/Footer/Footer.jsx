40a0ddbd2e9ad7d96a0996b69a793aababff54eb
youtipie18 <youtipie@gmail.com>
2024-06-11 22:14:32 +0300

    Fix bug where there is no selected item in footer when user on '/' route. Add 2 more images to footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,17 +4,19 @@ import trainIcon from "./images/train-icon.png";
 import profileIcon from "./images/profile-icon.png";
 import eatIcon from "./images/eat-icon.png";
 import rankIcon from "./images/rank-icon.png";
+import wholeIcon from "./images/whole-icon.png";
+import mentalIcon from "./images/mental-icon.png";
 import "./footer.css";
 import {Link, useLocation} from "react-router-dom";
 
 const footerItems = [
-    {img: eatIcon, route: "/temp", text: "temp"},
+    {img: mentalIcon, route: "/mental", text: "Mental"},
     {img: progressIcon, route: "/progress", text: "Progress"},
     {img: trainIcon, route: "/train", text: "Train"},
     {img: profileIcon, route: "/profile", text: "Profile"},
     {img: eatIcon, route: "/eat", text: "Eat"},
     {img: rankIcon, route: "/rank", text: "Rank"},
-    {img: eatIcon, route: "/temp", text: "temp"}
+    {img: wholeIcon, route: "/whole", text: "Whole"}
 
 ]
 
@@ -44,16 +46,19 @@ const Footer = () => {
         <section id="footer">
             <footer>
                 <div className="scroll-container">
-                    {footerItems.map(item =>
-                        <div key={item.route} className="footer-item">
-                            <Link
-                                className={"link" + (currentPage.pathname === item.route ? " selected" : "")}
-                                to={item.route}
-                            >
-                                <img src={item.img} alt={`${item.text} icon`}/>
-                                <p>{item.text}</p>
-                            </Link>
-                        </div>
+                    {footerItems.map(item => {
+                            const isSelected = currentPage.pathname === item.route || (currentPage.pathname === "/" && item.route === "/profile");
+
+                            return <div key={item.route} className="footer-item">
+                                <Link
+                                    className={"link" + (isSelected ? " selected" : "")}
+                                    to={item.route}
+                                >
+                                    <img src={item.img} alt={`${item.text} icon`}/>
+                                    <p>{item.text}</p>
+                                </Link>
+                            </div>
+                        }
                     )}
                 </div>
             </footer>
