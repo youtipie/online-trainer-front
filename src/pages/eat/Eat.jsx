a5ee646d384357ee2912c213fdec7c7f1dded93d
youtipie18 <youtipie@gmail.com>
2024-06-15 12:25:40 +0300

    Add eat page(Without calendar and one slide menu)

--- src/pages/eat/Eat.jsx
+++ src/pages/eat/Eat.jsx
@@ -1,15 +1,54 @@
 import React from 'react';
+import "./eat.css"
 import FixedSlideMenu from "../../UI/FixedSlideMenu/FixedSlideMenu";
+import DailyEatStats from "./components/DailyEatStats";
+import SlideMenu from "../../UI/SlideMenu/SlideMenu";
+import BodyStats from "./components/BodyStats";
+import BodyStatsProgress from "./components/BodyStatsProgress";
+
+const eatTimes = ["Сніданок", "Обід", "Вечеря", "Інше", "Трекер води", "Трекер сну"]
 
 const Eat = () => {
     return (
-        <>
-            <FixedSlideMenu
-                select={{name: "Test", subname: "Megatest"}}
-                options={[{name: "Test", subname: "Megatest"},{name: "Test1", subname: "Megatest1"}, {name: "Test2", subname: "Megatest2"}]}
-                onClick={(option) => console.log(option)}
-            />
-        </>
+        <div className="no-footer-overlap">
+            <div className="content-container overlap">
+                <div className="calendar-open">
+                    <FixedSlideMenu
+                        select={{name: "Розрахунок калорій на день"}}
+                        options={[{name: "Test", subname: "Megatest"}, {name: "Test1", subname: "Megatest1"}, {
+                            name: "Test2",
+                            subname: "Megatest2"
+                        }]}
+                        onClick={(option) => console.log(option)}
+                        selectStyles={{height: "4em", marginTop: "2em", width: "80%", fontSize: "0.8em"}}
+                        optionsStyles={{height: "20em"}}
+                    >
+                        <div style={{display: "flex", justifyContent: "center", alignItems: "center", height: "100%"}}>
+                            {/*TODO: Add real calendar*/}
+                            <h1>Nothing for now</h1>
+                        </div>
+                    </FixedSlideMenu>
+                </div>
+                <DailyEatStats header={"Щоденна норма"}/>
+            </div>
+            <div className="content-container">
+                <DailyEatStats header={"Щоденник харчування"}/>
+                <div className="eat-times">
+                    {eatTimes.map(time => (
+                        <SlideMenu
+                            key={time}
+                            select={{name: time}}
+                            options={[{name: "Сир - 500 кг"}]}
+                            selectStyles={{fontSize: "0.9em", height: "3em", width: "80%"}}
+                            optionsStyles={{width: "80%"}}
+                            iconStyles={{width: "2em"}}
+                        />
+                    ))}
+                </div>
+            </div>
+            <BodyStats/>
+            <BodyStatsProgress max={100} value={70}/>
+        </div>
     );
 };
 
