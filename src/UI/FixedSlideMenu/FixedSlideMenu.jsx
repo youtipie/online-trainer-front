0d8f699725a8ff1192e4b74d0a7420b78d5f0b21
youtipie18 <youtipie@gmail.com>
2024-06-15 12:23:59 +0300

    Remove div wrapper for FixedSlideMenu component. Add ability to apply some styles

--- src/UI/FixedSlideMenu/FixedSlideMenu.jsx
+++ src/UI/FixedSlideMenu/FixedSlideMenu.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import classes from "./FixedSlideMenu.module.css"
 import whiteArrowDown from "../../assets/images/white-arrow-down.png";
 
-const FixedSlideMenu = ({select, options, onClick}) => {
+const FixedSlideMenu = ({select, options, onClick, selectStyles, optionsStyles, optionStyles, children}) => {
+    // TODO: Add classes styles instead of inline styles
     const [isSelectVisible, setIsSelectVisible] = React.useState(false);
 
     const iconClasses = [classes["fixed-slide-menu-icon"], isSelectVisible ? classes["up"] : classes["down"]].join(" ");
@@ -19,46 +20,52 @@ const FixedSlideMenu = ({select, options, onClick}) => {
                 &&
                 <div className={classes["fixed-slide-menu-backdrop"]} onClick={() => setIsSelectVisible(false)}></div>
             }
-            <div className="content-container overlap">
-                <div className={classes["fixed-slide-menu-select"]}
-                     onClick={() => setIsSelectVisible(prev => !prev)}>
-                    <img
-                        className={iconClasses}
-                        src={whiteArrowDown}
-                        alt="Arrow down icon"
-                    />
-                    <div className={classes["fixed-slide-menu-select-text-wrapper"]}>
-                        <h1 className={classes.name}>{select.name}</h1>
-                        {
-                            select.subname
-                            &&
-                            <h2 className={classes.subname}>
-                                {select.subname}
-                            </h2>
-                        }
-                    </div>
-                    <img
-                        className={iconClasses}
-                        src={whiteArrowDown}
-                        alt="Arrow down icon"
-                    />
+            <div style={selectStyles} className={classes["fixed-slide-menu-select"] + " overlap"}
+                 onClick={() => setIsSelectVisible(prev => !prev)}>
+                <img
+                    className={iconClasses}
+                    src={whiteArrowDown}
+                    alt="Arrow down icon"
+                />
+                <div className={classes["fixed-slide-menu-select-text-wrapper"]}>
+                    <h1 className={classes.name}>{select.name}</h1>
+                    {
+                        select.subname
+                        &&
+                        <h2 className={classes.subname}>
+                            {select.subname}
+                        </h2>
+                    }
                 </div>
+                <img
+                    className={iconClasses}
+                    src={whiteArrowDown}
+                    alt="Arrow down icon"
+                />
             </div>
-            <div className={optionsClasses}>
-                {options.filter(item => item !== select).map(option =>
-                    <div key={option.name + option.subname} className={classes["fixed-slide-menu-option"]}
-                         onClick={() => handleClick(option)}>
-                        <div className={classes["fixed-slide-menu-select-text-wrapper"]}>
-                            <h1 className={classes["name"]}>{option.name}</h1>
-                            {
-                                option.subname
-                                &&
-                                <h2 className={classes.subname}>
-                                    {option.subname}
-                                </h2>
-                            }
-                        </div>
-                    </div>)
+            <div style={optionsStyles} className={optionsClasses}>
+                {
+                    children ?
+                        children
+                        :
+                        options.filter(item => item !== select).map(option =>
+                            <div
+                                style={optionStyles}
+                                key={option.name + option.subname}
+                                className={classes["fixed-slide-menu-option"]}
+                                onClick={() => handleClick(option)}
+                            >
+                                <div className={classes["fixed-slide-menu-select-text-wrapper"]}>
+                                    <h1 className={classes["name"]}>{option.name}</h1>
+                                    {
+                                        option.subname
+                                        &&
+                                        <h2 className={classes.subname}>
+                                            {option.subname}
+                                        </h2>
+                                    }
+                                </div>
+                            </div>)
                 }
             </div>
         </>
