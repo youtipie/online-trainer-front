5ffdd9db5aeaaae80e09afde7619a331715aead0
youtipie18 <youtipie@gmail.com>
2024-06-17 21:21:47 +0300

    Fix bug where using useIndexLocation on route '/' will give strange results

--- src/hooks/useIndexLocation.js
+++ src/hooks/useIndexLocation.js
@@ -2,9 +2,14 @@ import {useRef} from "react";
 import {useLocation} from "react-router-dom";
 import {routes} from "../router/routes";
 
-const useIndexLocation = () => {
+const useIndexLocation = (defaultRoute = "/profile") => {
     const location = useLocation();
     const route = useRef(location.pathname);
+
+    if (route.current === "/") {
+        route.current = defaultRoute;
+    }
+
     const index = useRef(0);
 
     routes.forEach((item, i) => {
